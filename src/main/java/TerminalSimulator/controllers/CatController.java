5247e7f7a2b9afaef0ae1467fa9c24cde4de6eef
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-29 12:56:44 -0300

    modify cat controller

--- src/main/java/TerminalSimulator/controllers/CatController.java
+++ src/main/java/TerminalSimulator/controllers/CatController.java
@@ -1,9 +1,17 @@
 package TerminalSimulator.controllers;
 
+import TerminalSimulator.models.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/cat")
 public class CatController {
+
+    @PostMapping
+    public ResponseEntity<Response> cat() {
+        return ResponseEntity.ok(new Response("cat"));
+    }
 }
