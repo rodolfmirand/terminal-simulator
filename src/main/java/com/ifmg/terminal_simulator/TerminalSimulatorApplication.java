85276823ae478bdbdbf7222cc5a4dd0f0e81adbc
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-29 08:22:08 -0300

    init project

---
+++ src/main/java/com/ifmg/terminal_simulator/TerminalSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ifmg.terminal_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifmg/terminal_simulator/TerminalSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifmg.terminal_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
