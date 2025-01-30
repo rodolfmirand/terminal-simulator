6ed538963ff8e9777e892416235b7ad693980d9f
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-29 22:10:27 -0300

    modify application main class

--- src/main/java/TerminalSimulator/Application.java
+++ src/main/java/TerminalSimulator/Application.java
@@ -1,12 +1,16 @@
 package TerminalSimulator;
 
+import TerminalSimulator.repository.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+    public static Database database;
+
     public static void main(String[] args) {
+        database = new Database();
         SpringApplication.run(Application.class, args);
     }
 
