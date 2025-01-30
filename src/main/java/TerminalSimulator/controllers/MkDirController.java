3ee0e73ba7c7f5bc2356323e53a5a32ea3f74a32
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-30 11:35:24 -0300

    modify mkdir controller

--- src/main/java/TerminalSimulator/controllers/MkDirController.java
+++ src/main/java/TerminalSimulator/controllers/MkDirController.java
@@ -1,9 +1,24 @@
 package TerminalSimulator.controllers;
 
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.MkDirService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/mkdir")
 public class MkDirController {
+
+    @Autowired
+    private MkDirService service;
+
+    @PostMapping
+        public ResponseEntity<Response> mkdir(@RequestBody Request request) {
+        return ResponseEntity.ok(this.service.execute(request));
+    }
 }
