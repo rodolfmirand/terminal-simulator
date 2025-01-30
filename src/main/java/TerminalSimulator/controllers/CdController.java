b396ac356d3f8c70c2e952ad65508be61270e686
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-29 22:08:00 -0300

    modify cd controller

--- src/main/java/TerminalSimulator/controllers/CdController.java
+++ src/main/java/TerminalSimulator/controllers/CdController.java
@@ -1,9 +1,23 @@
 package TerminalSimulator.controllers;
 
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.services.CdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/cd")
 public class CdController {
+
+    @Autowired
+    private CdService service;
+
+    @PostMapping(path = "/cd")
+    public ResponseEntity<String> cdController(@RequestBody Request request){
+        return ResponseEntity.ok(this.service.exectute(request));
+    }
 }
