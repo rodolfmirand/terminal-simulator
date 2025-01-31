89406aa80eb1f850e91a32dd5f8d3799f7a00c58
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-31 11:23:41 -0300

    modify cd service

--- src/main/java/TerminalSimulator/services/CdService.java
+++ src/main/java/TerminalSimulator/services/CdService.java
@@ -7,42 +7,45 @@ import TerminalSimulator.models.dto.response.Response;
 import TerminalSimulator.services.interfaces.CommandService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CdService implements CommandService {
 
     @Override
     public Response execute(Request request) {
-        if(request.args.length <= 1){
+        if (request.args.length <= 1) {
             return new Response("No args found", request.path);
         }
 
         String commandArg = request.args[1];
 
-        if(commandArg.equals("/")) return new Response("", "/");
+        if (commandArg.equals("/")) return new Response("", "/");
 
-        if(commandArg.contains("..")){
-            String[] dots = request.args[1].split("/");
-            String[] dirs = request.path.split("/");
-            if(dots.length >= dirs.length){
+        if (commandArg.contains("..")) {
+            ArrayList<String> dots = new ArrayList<>(List.of(request.args[1].split("/")));
+            ArrayList<String> dirs = new ArrayList<>(List.of(request.path.split("/")));
+            if (dots.size() >= dirs.size()) {
                 return new Response("", "root/");
             }
 
-            Directory currentDir = Application.database.findDirectory(dirs[dirs.length - 1]);
-            for (int i = dots.length; i > 0; i--){
+            Directory currentDir = Application.database.findDirectory(dirs);
+            for (int i = dots.size(); i > 0; i--) {
                 currentDir = currentDir.getParent();
             }
             return new Response("", currentDir.getPath());
         }
 
         Directory dir;
-        if(request.path.equals("/")){
+        if (request.path.equals("/")) {
             dir = Application.database.getRoot();
-        }else{
-            String[] path = request.path.split("/");
-            dir = Application.database.findDirectory(path[path.length - 1]);
+        } else {
+            ArrayList<String> path = new ArrayList<>(List.of(request.path.split("/")));
+            dir = Application.database.findDirectory(path);
         }
 
-        for(Directory d : dir.getDirectories()){
+        for (Directory d : dir.getDirectories()) {
             if (d.getName().equals(request.args[1])) {
                 return new Response("", d.getPath());
             }
