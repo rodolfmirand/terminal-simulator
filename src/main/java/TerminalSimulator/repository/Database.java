8b693be4e5e87e16f17a6a9325b9782f1ce503ef
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-01 15:57:10 -0300

    add current user atribute

--- src/main/java/TerminalSimulator/repository/Database.java
+++ src/main/java/TerminalSimulator/repository/Database.java
@@ -7,72 +7,83 @@ import java.util.ArrayList;
 
 public class Database {
 
-        private Directory root;
+    private Directory root;
 
-        public Database () {
-            this.root = new Directory("root/", "root", null);
+    private String currentUser;
+
+
+    public String getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public Database() {
+        this.root = new Directory("root/", "root", null);
+    }
+
+    public Directory getRoot() {
+        return root;
+    }
+
+    public void createDirectory(ArrayList<String> path, String name) {
+        Directory parent = findDirectory(root, path);
+        if (parent != null) {
+            parent.addDirectory(new Directory(parent.getPath(), name, parent));
+        } else {
+            System.out.println("Directory not found!");
         }
+    }
 
-        public Directory getRoot(){
-            return root;
+    public void createFile(ArrayList<String> path, String name, String data, String owner) {
+        Directory parent = findDirectory(root, path);
+        if (parent != null) {
+            parent.addFile(new File(parent.getPath(), name, data, owner));
         }
+    }
 
-        public void createDirectory(ArrayList<String> path, String name){
-            Directory parent = findDirectory(root, path);
-            if(parent != null){
-                parent.addDirectory(new Directory(parent.getPath(), name, parent));
-            }else{
-                System.out.println("Directory not found!");
+    public void removeDirectory(String name) {
+        if (root.getName().equals(name)) {
+            System.out.println("Impossible to remove root directory!");
+            return;
+        }
+        removeDirectory(root, name);
+    }
+
+    private void removeDirectory(Directory parent, String name) {
+        for (Directory childDir : parent.getDirectories()) {
+            if (childDir.getName().equals(name)) {
+                parent.removeDirectory(name);
             }
         }
+    }
 
-        public void createFile(ArrayList<String> path, String name, String data) {
-            Directory parent = findDirectory(root, path);
-            if(parent != null){
-                parent.addFile(new File(parent.getPath(), name, data));
+    public void removeFile(ArrayList<String> path, String name) {
+        Directory parent = findDirectory(root, path);
+        for (File file : parent.getFiles()) {
+            if (file.getName().equals(name)) {
+                parent.removeFile(name);
             }
         }
+    }
 
-        public void removeDirectory(String name){
-            if(root.getName().equals(name)){
-                System.out.println("Impossible to remove root directory!");
-                return;
-            }
-            removeDirectory(root, name);
+    public Directory findDirectory(ArrayList<String> path) {
+        return findDirectory(root, path);
+    }
+
+    private Directory findDirectory(Directory current, ArrayList<String> path) {
+        if (path.size() == 1) {
+            return current;
         }
-
-        private void removeDirectory(Directory parent, String name){
-            for(Directory childDir : parent.getDirectories()){
-                if(childDir.getName().equals(name)){
-                    parent.removeDirectory(name);
-                }
-            }
-        }
-
-        public void removeFile(ArrayList<String> path, String name){
-            Directory parent = findDirectory(root, path);
-            for(File file : parent.getFiles()){
-                if(file.getName().equals(name)){
-                    parent.removeFile(name);
-                }
+        for (Directory child : current.getDirectories()) {
+            if (child.getName().equals(path.get(1))) {
+                path.removeFirst();
+                return findDirectory(child, path);
             }
         }
-
-        public Directory findDirectory(ArrayList<String> path){
-            return findDirectory(root, path);
-        }
-
-        private Directory findDirectory(Directory current, ArrayList<String> path){
-            if(path.size() == 1){
-                return current;
-            }
-            for(Directory child : current.getDirectories()){
-                if(child.getName().equals(path.get(1))){
-                    path.removeFirst();
-                    return findDirectory(child, path);
-                }
-            }
-            return null;
-        }
+        return null;
+    }
 
 }
