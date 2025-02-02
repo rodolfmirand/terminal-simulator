7f9cb37775ace6ffab2be9c372d9b3d545d4ec6a
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-02 12:19:47 -0300

    implements touch command

--- src/main/java/TerminalSimulator/controllers/TouchController.java
+++ src/main/java/TerminalSimulator/controllers/TouchController.java
@@ -1,4 +1,26 @@
 package TerminalSimulator.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.TouchService;
+
+@RestController
+@RequestMapping(path = "/touch")
 public class TouchController {
+	
+	@Autowired
+	private TouchService service;
+	
+	@PostMapping()
+    public ResponseEntity<Response> touch(@RequestBody Request request){
+		System.out.print("Entrei");
+        return ResponseEntity.ok(this.service.execute(request));
+    }
 }
--- src/main/java/TerminalSimulator/services/TouchService.java
+++ src/main/java/TerminalSimulator/services/TouchService.java
@@ -1,4 +1,31 @@
 package TerminalSimulator.services;
 
-public class TouchService {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import TerminalSimulator.Application;
+import TerminalSimulator.models.Directory;
+import TerminalSimulator.models.File;
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.interfaces.CommandService;
+
+@Service		
+public class TouchService implements CommandService{
+
+	@Override
+	public Response execute(Request request) {
+		if (request.args.length < 1) {
+            return new Response("No args found", request.path);
+        }
+		
+		Directory currentDir = Application.database.findDirectory(new ArrayList<>(List.of(request.path.split("/"))));
+		File emptyFile = new File(request.path, request.args[0], Application.database.getCurrentUser());
+		
+		currentDir.addFile(emptyFile);
+		
+		return new Response("Empty file created with success!", request.path);
+	}
 }
