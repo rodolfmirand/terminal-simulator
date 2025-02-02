b8602a3fe682f119c9a6b9371565a105b02f3d10
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-02 12:21:37 -0300

    implements find command

--- src/main/java/TerminalSimulator/controllers/FindController.java
+++ src/main/java/TerminalSimulator/controllers/FindController.java
@@ -1,4 +1,27 @@
 package TerminalSimulator.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.FindService;
+import TerminalSimulator.services.TouchService;
+
 public class FindController {
+	@RestController
+	@RequestMapping(path = "/find")
+	public class TouchController {
+		
+		@Autowired
+		private FindService service;
+		
+		@PostMapping()
+	    public ResponseEntity<Response> find(@RequestBody Request request){
+	        return ResponseEntity.ok(this.service.execute(request));
+	    }
 }
--- src/main/java/TerminalSimulator/services/FindService.java
+++ src/main/java/TerminalSimulator/services/FindService.java
@@ -1,4 +1,37 @@
 package TerminalSimulator.services;
 
-public class FindService {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import TerminalSimulator.Application;
+import TerminalSimulator.models.Directory;
+import TerminalSimulator.models.File;
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.interfaces.CommandService;
+
+@Service
+public class FindService implements CommandService {
+
+	@Override
+	public Response execute(Request request) {
+		if(request.args.length < 4) {
+			return new Response("No args found", request.path);
+        }
+		
+		if(request.args[2] != "-name") {
+			return new Response("The -name flag is mandatory" , request.path);
+		}
+		
+		Directory currentDir = Application.database.findDirectory(new ArrayList<>(List.of(request.args[1])));
+		File file = currentDir.findFile(request.args[3]);
+		
+		if(file == null) {
+			return new Response("The file named as " + request.args[3] + "doesn't exists", null);
+		}
+		
+		return new Response("The file named as " + request.args[3] + "was found", null);
+	}
 }
