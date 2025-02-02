7af04215316dd1de041a731554b56a1287500480
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-02 12:20:12 -0300

    implements echo command

--- src/main/java/TerminalSimulator/controllers/EchoController.java
+++ src/main/java/TerminalSimulator/controllers/EchoController.java
@@ -1,9 +1,24 @@
 package TerminalSimulator.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.EchoService;
 
 @RestController
 @RequestMapping(path = "/echo")
 public class EchoController {
+	@Autowired
+	private EchoService service;
+	
+	@PostMapping
+    public ResponseEntity<Response> rmDir(@RequestBody Request request){
+        return ResponseEntity.ok(this.service.execute(request));
+    }
 }
--- src/main/java/TerminalSimulator/services/EchoService.java
+++ src/main/java/TerminalSimulator/services/EchoService.java
@@ -1,4 +1,51 @@
 package TerminalSimulator.services;
 
-public class EchoService {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import TerminalSimulator.Application;
+import TerminalSimulator.models.Directory;
+import TerminalSimulator.models.File;
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.interfaces.CommandService;
+
+@Service	
+public class EchoService implements CommandService {
+
+	@Override
+	public Response execute(Request request) {
+		if(request.args.length < 4) {
+			return new Response("No args found", request.path);
+        }
+		
+		Directory currentDir = Application.database.findDirectory(new ArrayList<>(List.of(request.args[2])));
+		File file = currentDir.findFile(request.args[3]);
+		
+		if(file == null) {
+			return new Response("The file named as " + request.args[3] + " doesn't exists", null);
+		}
+		
+		return switch(request.args[2]) {
+			case ">" -> this.overwriteFileContent(file, request.args[1], request.path);
+			case ">>"->  this.concatContentToFile(file, request.args[1], request.path);
+			default -> new Response("The > or >> flag are missing", request.path);
+		};
+	}
+	
+	private Response overwriteFileContent(File file, String content, String path) {
+		file.setData(content);
+		file.setBytesSize();
+		
+		return new Response("New content added to file", path);
+	}
+	
+	private Response concatContentToFile(File file, String content, String path) {
+		file.setData(file.getData() + content);
+		file.setBytesSize();
+		
+		return new Response("New content concatenated to file", path);
+	}
 }
