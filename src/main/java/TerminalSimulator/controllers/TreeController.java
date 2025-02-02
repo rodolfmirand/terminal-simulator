d2fd90175247fcd13f5aec202f389bc13440b293
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-02 15:10:10 -0300

    add tree controller

--- src/main/java/TerminalSimulator/controllers/TreeController.java
+++ src/main/java/TerminalSimulator/controllers/TreeController.java
@@ -1,4 +1,20 @@
 package TerminalSimulator.controllers;
 
+import TerminalSimulator.Application;
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/tree")
 public class TreeController {
+
+    @PostMapping
+    public ResponseEntity<String> tree() {
+        return ResponseEntity.ok().body(Application.database.tree());
+    }
 }
