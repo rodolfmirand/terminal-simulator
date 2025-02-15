3bb3586e4dab5e50c63af2da42bdf59e5bd72d1a
Willian Bortolini <william_bortolini@cp3tecnologia.com.br>
2025-02-15 07:44:33 -0300

    stat implementado

--- src/main/java/TerminalSimulator/services/StatService.java
+++ src/main/java/TerminalSimulator/services/StatService.java
@@ -1,4 +1,41 @@
 package TerminalSimulator.services;
 
-public class StatService {
+
+import TerminalSimulator.Application;
+import TerminalSimulator.models.Directory;
+import TerminalSimulator.models.File;
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.interfaces.CommandService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class StatService implements CommandService {
+
+    @Override
+    public Response execute(Request request) {
+        if (request.args.length < 2) {
+            return new Response("Usage: stat <name>", request.path);
+        }
+
+        String name = request.args[1];
+        Directory currentDir = Application.database.findDirectory(new ArrayList<>(List.of(request.path.split("/"))));
+
+        if (currentDir == null) {
+            return new Response("Directory not found", request.path);
+        }
+
+        File file = currentDir.findFile(name);
+        Directory dir = currentDir.findChildDirectory(name);
+
+        if (file != null) {
+            return new Response(file.toStringLs(), request.path);
+        } else if (dir != null) {
+            return new Response(dir.toStringLs(), request.path);
+        } else {
+            return new Response("File or directory not found", request.path);
+        }
+    }
 }
