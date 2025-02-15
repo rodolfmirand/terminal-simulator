767853a269fe4d4ef5fdd0408f8cc6ae03150ec8
EdOc-PS <eeuardooctavio@gmail.com>
2025-02-15 13:25:15 -0300

    add head

--- src/main/java/TerminalSimulator/controllers/HeadController.java
+++ src/main/java/TerminalSimulator/controllers/HeadController.java
@@ -1,4 +1,23 @@
 package TerminalSimulator.controllers;
 
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.HeadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/head")
 public class HeadController {
+    @Autowired
+    private HeadService service;
+
+    @PostMapping
+    public ResponseEntity<Response> head(@RequestBody Request request) {
+        return ResponseEntity.ok(this.service.execute(request));
+    }
 }
--- src/main/java/TerminalSimulator/services/HeadService.java
+++ src/main/java/TerminalSimulator/services/HeadService.java
@@ -1,4 +1,47 @@
 package TerminalSimulator.services;
 
-public class HeadService {
+import TerminalSimulator.Application;
+import TerminalSimulator.models.Directory;
+import TerminalSimulator.models.File;
+import TerminalSimulator.models.dto.request.Request;
+import TerminalSimulator.models.dto.response.Response;
+import TerminalSimulator.services.interfaces.CommandService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HeadService implements CommandService {
+    @Override
+    public Response execute(Request request) {
+        if (request.args.length < 2) {
+            return new Response("No file specified", request.path);
+        }
+
+        int linesToShow;
+
+        try {
+            linesToShow = Integer.parseInt(request.args[2]);
+            if (linesToShow < 1) {
+                return new Response("Invalid number of lines", request.path);
+            }
+        } catch (NumberFormatException e) {
+            return new Response("Invalid number format", request.path);
+        }
+
+        Directory currentDir = Application.database.findDirectory(new ArrayList<>(List.of(request.path.split("/"))));
+        File file = currentDir.findFile(request.args[1]);
+        if (file == null) {
+            return new Response("File not found: " + request.args[1], request.path);
+        }
+
+        String[] lines = file.getData().split("\n");
+        StringBuilder output = new StringBuilder("Head of file: " + request.args[1] + "\n");
+        for (int i = 0; i < Math.min(linesToShow, lines.length); i++) {
+            output.append(lines[i]).append("\n");
+        }
+
+        return new Response(output.toString(), request.path);
+    }
 }
